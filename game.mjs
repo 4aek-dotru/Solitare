09f2f8f37e18bba85a0ec06731e830f15254cb43
4aek-dotru <andrey12112005@yandex.ru>
2026-04-11 16:27:39 +0300

    Add files via upload

--- game.mjs
+++ game.mjs
@@ -2,15 +2,23 @@ const closeCardsContainer = document.getElementById('close-cards');
 const openCardsContainer = document.getElementById('open-cards');
 const cardSlotsContainer = document.querySelectorAll('.card-slot');
 const gameCellsContainer = document.querySelectorAll('.game-cell');
+const suits = [
+    { name: "Clubs", group: "BLACK" },
+    { name: "Spades", group: "BLACK" },
+    { name: "Hearts", group: "RED" },
+    { name: "Diamonds", group: "RED" }
+];
+const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
 
 export default class Game {
-    CARDS = [];
+    CARDS_SETTINGS = {};
     CURRENT_CARD;
     CURRENT_CONTAINER;
     CURRENT_CORDS_CARD;
     CURRENT_CORDS_MOUSE;
     constructor() {
         this.createCards();
+        closeCardsContainer.addEventListener('click', this.returnCards.bind(this))
     }
 
     createCards() {
@@ -24,21 +32,21 @@ export default class Game {
             rowCard.ondragstart = function() {
                 return false;
             };
-            this.mouseDown(rowCard);
+            this.mouseDownHandler = (e) => this.mouseDown(rowCard, e);
+            rowCard.addEventListener('mousedown', this.mouseDownHandler);
         }
+        this.randomCardsSettings()
     }
-    mouseDown(card) {
-        card.addEventListener('mousedown', (e) => {
-            this.CURRENT_CORDS_MOUSE = e;
-            this.CURRENT_CONTAINER = card.parentNode;
-            if(card.classList.contains('open')) {
-                this.startDrag(card);
-            }else {
-                if(card.parentNode == closeCardsContainer) {
-                    this.moveToOpenContainer(card);
-                }
+    mouseDown(card, e) {
+        this.CURRENT_CORDS_MOUSE = e;
+        this.CURRENT_CONTAINER = card.parentNode;
+        if(card.classList.contains('open')) {
+            this.startDrag(card);
+        }else {
+            if(card.parentNode == closeCardsContainer) {
+                this.moveToOpenContainer(card);
             }
-        })
+        }
     }
     moveToOpenContainer(card) {
         let clone = card.cloneNode();
@@ -52,16 +60,33 @@ export default class Game {
         card.style.opacity = '0';
         card.style.pointerEvents = 'none';
         clone.style.transform = `translate(${targetCoordinates.left - this.CURRENT_CORDS_CARD.left}px, ${targetCoordinates.top - this.CURRENT_CORDS_CARD.top})`
-        card.classList.add('open');
     
         setTimeout(() => {
             clone.remove();
             openCardsContainer.appendChild(card);
-            card.classList.remove('close');
             card.style.opacity = '1';
             card.style.pointerEvents = 'auto';
+            this.openCard(card);
+            this.checkColumn(openCardsContainer, false)
+            this.checkColumn(closeCardsContainer, false)
         }, 200)
-        
+    }
+    openCard(card) {
+        card.classList.remove('close');
+        card.classList.add('open');
+        this.addRankAndSuit(card);
+    }
+    closeCard() {
+        const allChildrens = closeCardsContainer.childNodes;
+        for (let i = allChildrens.length - 1; i > -1; i--) {
+            allChildrens[i].innerHTML = '';
+            allChildrens[i].style.backgroundColor = '#808080';
+            allChildrens[i].classList.remove('open');
+            allChildrens[i].classList.add('close');
+            allChildrens[i].dataset.suitGroup = '';
+            allChildrens[i].dataset.suit = '';
+            allChildrens[i].dataset.rank = '';
+        }
     }
     startDrag(card) {
         let clone = card.cloneNode();
@@ -87,17 +112,14 @@ export default class Game {
         const centerCloneY = cloneRect.top + cloneRect.height / 2;
         const elementUnderClone = document.elementsFromPoint(centerCloneX, centerCloneY)[1];
         let parent;
+
         if(elementUnderClone.classList.contains('card')) parent = elementUnderClone.parentNode;
             else parent = elementUnderClone;
         if(parent.dataset.dragContainer != '1' && parent.dataset.dragContainer != '2') {
             this.cancelDrag(clone, card);
             return
         }
-        parent.appendChild(card);
-        clone.remove();
-        parent.dataset.dragContainer == '1' ? this.checkColumn(parent, true) : this.checkColumn(parent, false)
-        card.style.opacity = '1';
-        this.CURRENT_CONTAINER.dataset.dragContainer == '1' ? this.checkColumn(this.CURRENT_CONTAINER, true) : this.checkColumn(this.CURRENT_CONTAINER, false);
+        this.checkCardsInCell(card, parent, clone)
     }
     cancelDrag(clone, card) {
         clone.remove();
@@ -122,5 +144,72 @@ export default class Game {
         let differentX = this.CURRENT_CORDS_MOUSE.pageX - e.pageX;
         clone.style.left = this.CURRENT_CORDS_CARD.left - differentX + 'px';
         clone.style.top = this.CURRENT_CORDS_CARD.top - differentY + 'px';
+    }
+    randomCardsSettings() {
+        let cardId = 1;
+        for(const suit of suits) {
+            for(const rank of ranks) {
+                this.CARDS_SETTINGS[cardId] = {
+                    RANK : rank,
+                    SUIT : suit.name,
+                    SUIT_GROUP : suit.group
+                }
+                cardId++;
+            }
+        }
+        const entries = Object.entries(this.CARDS_SETTINGS);
+        for (let i = entries.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [entries[i], entries[j]] = [entries[j], entries[i]];
+        }
+        const shuffledObj = {};
+        for (let i = 0; i < entries.length; i++) {
+            shuffledObj[i + 1] = entries[i][1];
+        }
+        this.CARDS_SETTINGS = shuffledObj;
+    }
+    addRankAndSuit(card) {
+        card.dataset.rank = this.CARDS_SETTINGS[Number(card.dataset.id) + 1].RANK;
+        card.dataset.suit = this.CARDS_SETTINGS[Number(card.dataset.id) + 1].SUIT;
+        card.dataset.suitGroup = this.CARDS_SETTINGS[Number(card.dataset.id) + 1].SUIT_GROUP;
+        card.style.backgroundColor = `${card.dataset.suitGroup}`;
+        if(card.dataset.suitGroup == "BLACK") card.style.color = "white"
+        card.innerHTML = `${card.dataset.rank}`;
+    }
+    checkCardsInCell(card, parent, clone) {
+        const childs = parent.childNodes;
+        const lastChild = childs[childs.length - 1]
+        if(lastChild != undefined) {
+            if(card.dataset.suitGroup != lastChild.dataset.suitGroup && Number(lastChild.dataset.rank) - Number(card.dataset.rank) == 1) {
+                parent.appendChild(card);
+                parent.dataset.dragContainer == '1' ? this.checkColumn(parent, true) : this.checkColumn(parent, false)
+                card.style.opacity = '1';
+                this.CURRENT_CONTAINER.dataset.dragContainer == '1' ? this.checkColumn(this.CURRENT_CONTAINER, true) : this.checkColumn(this.CURRENT_CONTAINER, false);
+                clone.remove();
+            }else {
+                this.cancelDrag(clone, card)
+            }
+        }else {
+            if(Number(card.dataset.rank) == 13) {
+                parent.appendChild(card);
+                parent.dataset.dragContainer == '1' ? this.checkColumn(parent, true) : this.checkColumn(parent, false)
+                card.style.opacity = '1';
+                this.CURRENT_CONTAINER.dataset.dragContainer == '1' ? this.checkColumn(this.CURRENT_CONTAINER, true) : this.checkColumn(this.CURRENT_CONTAINER, false);
+                clone.remove()
+            }else {
+                this.cancelDrag(clone, card)
+            }
+        }
+    }
+    returnCards() {
+        if(closeCardsContainer.children.length == 0){
+            const allChildrens = openCardsContainer.childNodes;
+            for (let i = allChildrens.length - 1; i > -1; i--) {
+                closeCardsContainer.appendChild(allChildrens[i])
+            }
+
+            this.closeCard();
+            this.checkColumn(closeCardsContainer, false)
+        }
     }
 }
